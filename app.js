afb4fc7b17d41cd93ac814834fd13d259807b4e4
Ahmed Zubair <153666614+AhmedZubair18@users.noreply.github.com>
2024-12-17 23:54:04 +0500

    Modal

---
+++ app.js
@@ -0,0 +1,9 @@
+function openModal(){
+   let open = document.querySelector(".container").style.display = "none"
+   let close = document.querySelector(".containers").style.display = "flex"
+    
+}
+function  closeModal(){
+    let close = document.querySelector(".containers").style.display = "none"
+    let open = document.querySelector(".container").style.display = "flex"
+}
